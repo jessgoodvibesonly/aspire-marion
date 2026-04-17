958c81d20e0ef70fdb8646dd6c679a68b880d9f3
Jessgoodvibesonly <jessgoodvibesonly@gmail.com>
2026-04-17 16:45:36 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
+import type { ReactNode } from "react";
+
 export const metadata = {
   title: "Aspire PODS | Marion",
-  description: "Aspire PODS overview for Marion"
-}
+  description: "Aspire PODS overview for Marion",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode;
 }) {
   return (
     <html lang="en">
       <body style={{ margin: 0 }}>{children}</body>
     </html>
-  )
+  );
 }
