eb6fc34f66d1d47df3ccb9186f1d36b29789e159
Jessgoodvibesonly <jessgoodvibesonly@gmail.com>
2026-04-17 15:02:22 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Aspire PODS | Marion",
+  description: "Aspire PODS overview for Marion"
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  )
+}
